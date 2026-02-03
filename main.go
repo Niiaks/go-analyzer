171fbe5ebb410a80b9d60d10cd75f7d5c9adcebe
Niiaks <napappoe007@st.ug.edu.gh>
2026-02-03 22:49:16 +0000

    Refactor file scanning logic and improve concurrency handling

--- main.go
+++ main.go
@@ -1,61 +1,70 @@
 package main
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
 	"fmt"
-	"io"
 	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
+	"sync"
 )
 
 const Size = 3 * 1 << 30 // 3 gigabytes
 
 func main() {
-	root := "C:/Users/junio"
+	dir := "C:/Users/junio"
+	fmt.Printf("Scanning files is dir %s\n", dir)
 
-	fmt.Printf("Scanning directory %s\n", root)
-	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+	//a channel which handles all paths
+	jobs := make(chan string, 200)
+
+	var wg sync.WaitGroup
+
+	workerCounter := 4
+
+	for range workerCounter {
+		wg.Add(1)
+
+		wg.Go(func() {
+			defer wg.Done()
+			for path := range jobs {
+				checkFile(path)
+			}
+		})
+	}
+
+	err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
 
-		fileInfo, err := os.Stat(path)
-		if err != nil {
-			return err
-		}
-
-		if fileInfo.Size() > Size {
-			fmt.Printf("the file name is %s, located at %s with size %d\n", d.Name(), path, fileInfo.Size())
+		if !d.IsDir() {
+			jobs <- path
 		}
 
 		return nil
 
 	})
-
-	fmt.Println("files not large in current directory")
+	close(jobs)
+	wg.Wait()
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println("Scan complete.")
 }
 
-func hashFile(filePath string) (string, error) {
-	file, err := os.Open(filePath)
+// checkFile prints the file and it's size
+func checkFile(path string) {
+	info, err := os.Stat(path)
 	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	hash := sha256.New()
-
-	//stream file contents into the hash
-	if _, err := io.Copy(hash, file); err != nil {
-		return "", err
+		return
 	}
 
-	hashInBytes := hash.Sum(nil)
-	return hex.EncodeToString(hashInBytes), nil
+	if info.Size() > Size {
+		fmt.Printf("Large file: %s | Size: %.2f GB\n",
+			path,
+			float64(info.Size())/(1<<30))
+	}
 }
