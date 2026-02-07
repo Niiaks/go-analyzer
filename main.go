8c9835e54d8d05763864d457bb0e22e848598e7c
Niiaks <napappoe007@st.ug.edu.gh>
2026-02-07 12:13:20 +0000

    Refactor job handling in main function and update checkFile signature

--- main.go
+++ main.go
@@ -18,6 +18,11 @@ func usage() {
 	os.Exit(2)
 }
 
+type Job struct {
+	path string
+	d    fs.DirEntry
+}
+
 func main() {
 
 	// dir is the directory to start scanning from (flag "-dir", default: ".")
@@ -26,30 +31,29 @@ func main() {
 
 	// minSize is the minimum file size threshold (in gigabytes) used to decide whether a file
 	// should be reported as "large". Set with -s; files with size > minSize are reported.
-	minSize := flag.Int("s", 1, "minimum file size in GB")
-	minBytes := int64(*minSize) << 30
+	minSize := flag.Float64("s", 1, "minimum file size in GB")
 
 	flag.Usage = usage
 	flag.Parse()
 
+	minBytes := int64(*minSize) << 30
 	fmt.Printf("Scanning files in dir %s\n", *dir)
 
 	//a channel which handles all paths
-	jobs := make(chan string, 200)
+
+	jobs := make(chan Job, 200)
 
 	var wg sync.WaitGroup
 
 	workerCounter := 4
 
 	for range workerCounter {
-		wg.Add(1)
 
-		go func() {
-			defer wg.Done()
-			for path := range jobs {
-				checkFile(path, minBytes)
+		wg.Go(func() {
+			for job := range jobs {
+				checkFile(job.path, job.d, minBytes)
 			}
-		}()
+		})
 	}
 
 	err := filepath.WalkDir(*dir, func(path string, d fs.DirEntry, err error) error {
@@ -57,8 +61,13 @@ func main() {
 			return err
 		}
 
+		job := Job{
+			path: path,
+			d:    d,
+		}
+
 		if !d.IsDir() {
-			jobs <- path
+			jobs <- job
 		}
 
 		return nil
@@ -75,8 +84,8 @@ func main() {
 }
 
 // checkFile prints the file and it's size
-func checkFile(path string, size int64) {
-	info, err := os.Stat(path)
+func checkFile(path string, d fs.DirEntry, size int64) {
+	info, err := d.Info()
 	if err != nil {
 		return
 	}
